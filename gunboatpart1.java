af5de9774da3c67fdddcced9502e89f9dbc83805
Vaishnavi-Josyula <43019257+Vaishnavi-Josyula@users.noreply.github.com>
2020-08-17 14:38:06 -0700

    Update gunboatpart1.java

--- gunboatpart1.java
+++ gunboatpart1.java
@@ -4,6 +4,9 @@ public class Main
 {
 	public static void main(String[] args) 
   {
+    //the following code takes in the distance(m), height(m), and inital velocity(m/s), and outputs the higher angle(degrees)
+			
+    //this chunk of code takes in the three paramenters listed above and stores them
     Scanner reader = new Scanner(System.in);
     System.out.print("Enter distance to target (in m): \n"); 
     double d = reader.nextDouble();
@@ -11,11 +14,16 @@ public class Main
     double h = reader.nextDouble();
     System.out.print("Enter the initial velocity (in m/s):  \n"); 
     double u = reader.nextDouble();
+		
+    //here, we're plugging in values into the formula to get theta	
     double a = (-4.9)*(d/u)*(d/u);
+		//b = d, so I didn't create a new variable and chose to stick with d
     double c = a+h;
     double first_answer = Math.atan((-d + Math.sqrt(d*d- 4*a*c))/(2*a));
+    /* double second_answer = Math.atan((-b - Math.sqrt((d*d)- 4*a*c))/2*a);
+     second_answer is ignored as per instructions */
+    // since Java stores angles in radians and we want the answer in degrees, we multiply first_answer by 180/pi
     double final_answer = (first_answer*180)/3.1415926536;
     System.out.println("The angle is  " + final_answer);
-   // double second_answer = Math.atan((-b - Math.sqrt((d*d)- 4*a*c))/2*a);
 	}
 }
